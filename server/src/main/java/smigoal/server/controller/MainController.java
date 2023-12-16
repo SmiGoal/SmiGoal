6a884fc1dfe5abd492da70c852e0e536ddaf75a2
ajroot5685 <anjeong8577@naver.com>
2023-12-16 16:18:27 +0900

    feat: MainController 구성

--- server/src/main/java/smigoal/server/controller/MainController.java
+++ server/src/main/java/smigoal/server/controller/MainController.java
@@ -19,12 +19,15 @@ public class MainController {
 
     @PostMapping("")
     public String smishingCheck(@RequestBody QuestionDTO request){
+        System.out.println(request.url);
+        System.out.println(request.message);
         String filterResult;
         if (request.url==null && request.message==null){
             return "error";
         }else if(request.url!=null){
             String urlContent = crawlingService.getURLContent(request.url);
-            String keyward = chatService.getChatResponse(urlContent);
+            String keyward = chatService.getChatResponse(urlContent.substring(0,300));  // 조절 필요
+//            System.out.println(keyward);
             filterResult = "true";  // 모델과 통신하여 필터링 결과 저장
         }else{
             String keyward = chatService.getChatResponse(request.message);
