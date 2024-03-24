c9f07e879ab7f4b7a05f2480100b10a81288cbb0
ajroot5685 <anjeong8577@naver.com>
2024-03-24 22:13:20 +0900

    release: 모델 파일 경로 수정

--- model/src/s3upload.py
+++ model/src/s3upload.py
@@ -11,7 +11,7 @@ s3 = boto3.client('s3',
                   aws_access_key_id=access_key_id,
                   aws_secret_access_key=secret_access_key)
 
-model_dir = './src/model_100000_data.pt'
+model_dir = './model_100000_data.pt'
 bucket_name = 'smigoal'
 s3_dir = 'model/model_100000_data.pt'
 
