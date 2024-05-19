c69ffb426584e14ae67dd114d902e40e88abaefc
ajroot5685 <anjeong8577@naver.com>
2024-05-19 15:31:48 +0900

    feat: 파일 정리 및 cuda 사용 위한 설정 추가

--- model/src/app.py
+++ model/src/app.py
@@ -3,7 +3,7 @@ from flask import Flask, request, jsonify
 import torch
 import torch.nn as nn
 import numpy as np
-from transformers import AlbertForSequenceClassification, AlbertTokenizer
+from transformers import RobertaModel, RobertaConfig, RobertaTokenizer
 
 from keyExtraction import extract_meaningful_words as extract
 
@@ -34,30 +34,67 @@ class AnalysisResult:
         self.spam_percentage = spam_percentage
         self.result = result
 
-class ALBertTextClassifier(nn.Module):
-    def __init__(self, num_labels=2):
-        super(ALBertTextClassifier, self).__init__()
-        self.albert = AlbertForSequenceClassification.from_pretrained('albert-base-v2', num_labels=num_labels)
+class RobertaTextClassifier(nn.Module):
+    def __init__(self, num_labels=2, dropout_prob=0.2):
+        super(RobertaTextClassifier, self).__init__()
+
+        # RoBERTa 모델 및 설정 불러오기
+        config = RobertaConfig.from_pretrained('roberta-base')
+        self.roberta = RobertaModel.from_pretrained('roberta-base', config=config)
+
+        # 은닉층의 크기는 config에서 가져옵니다.
+        self.hidden_size = config.hidden_size
+
+        # 라벨의 개수
+        self.num_labels = num_labels
+
+        # 활성화 함수 변경 (예: ReLU)
+        self.activation = nn.ReLU()
+
+        # 첫 번째 선형 레이어: 입력 크기는 hidden_size, 출력 크기는 hidden_size로 설정
+        self.linear1 = nn.Linear(in_features=self.hidden_size, out_features=self.hidden_size)
+
+        # 두 번째 선형 레이어: 입력 크기는 hidden_size, 출력 크기는 num_labels로 설정
+        self.linear2 = nn.Linear(in_features=self.hidden_size, out_features=self.num_labels)
+
+        # 드롭아웃 레이어 추가
+        self.dropout1 = nn.Dropout(dropout_prob)
+        self.dropout2 = nn.Dropout(dropout_prob)
 
     def forward(self, input_ids, attention_mask):
-        outputs = self.albert(input_ids, attention_mask=attention_mask)
-        logits = outputs.logits
-        return logits
+        # RoBERTa 모델의 출력 받기
+        outputs = self.roberta(input_ids=input_ids, attention_mask=attention_mask)
+
+        # Pooler 출력에서 [CLS] 토큰에 해당하는 벡터 추출
+        cls_vector = outputs.pooler_output
+
+        # 첫 번째 선형 레이어 및 드롭아웃 적용
+        cls_vector = self.linear1(cls_vector)
+        cls_vector = self.activation(cls_vector)
+        cls_vector = self.dropout1(cls_vector)
+
+        # 두 번째 선형 레이어 및 드롭아웃 적용
+        cls_vector = self.linear2(cls_vector)
+        cls_vector = self.dropout2(cls_vector)
+
+        return cls_vector
+
     
-def load_pretrained_albert_weights(model, model_path):
+def load_pretrained_roberta_weights(model, model_path):
     # 미리 학습된 ALBERT 모델 로드
-    pretrained_model = torch.load(model_path, map_location=torch.device('cpu'))
+    pretrained_model = torch.load(model_path)
 
     # ALBERT 모델의 가중치를 현재 모델로 복사
     model.load_state_dict(pretrained_model['model_state_dict'])
-
+    
 def test_model(model_path, keyword_list):
+        
     # 모델 로드
-    loaded_model = ALBertTextClassifier(num_labels=2)
-    load_pretrained_albert_weights(loaded_model, model_path)
+    loaded_model = RobertaTextClassifier(num_labels=2)
+    load_pretrained_roberta_weights(loaded_model, model_path)
 
     # 토크나이저 로드
-    tokenizer = AlbertTokenizer.from_pretrained('albert-base-v2')
+    tokenizer = RobertaTokenizer.from_pretrained('roberta-base')
 
     # 입력 데이터 토큰화
     input_text = ' '.join(keyword_list)
@@ -86,8 +123,8 @@ def test_model(model_path, keyword_list):
 
     return result_object
 
-model_path = "/app/src/AlbertForSequenceClassification.pt"
-# model_path = "./AlbertForSequenceClassification.pt"
+model_path = "/app/src/RoBERTa-base.pt"
+# model_path = "./RoBERTa-base.pt"
 
 if __name__ == '__main__':
     app.run(debug=True)
